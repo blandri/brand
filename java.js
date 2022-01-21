a1c6b38f1d5b060eface1bfd7d5a018c9a534702
blandri <landrybrok3@gmail.com>
2022-01-21 10:47:22 +0200

    ft:addition of crud operations and queries

--- java.js
+++ java.js
@@ -1,8 +1,6 @@
 let body = document.getElementById('body');
 let del = document.querySelector('.fa-trash-alt');
-
-let post = document.querySelector('.pos');
-
+let date = document.querySelector('.date');
 let picture = document.querySelector('.image');
 let par = document.querySelector('#article');
 let articl = picture.parentNode;
@@ -13,11 +11,14 @@ let content = document.querySelector('.content');
 let likes = document.querySelector('.lik');
 let a = ar.childNodes;
 
+//creating article
+
+console.log(localStorage.new);
 function nullP(mes, me) {
   title.innerHTML = mes;
   content.innerHTML = me;
 }
-console.log(par.childNodes);
+
 //Likes operation
 let value = JSON.parse(localStorage.getItem('likes'));
 likes.innerHTML = value;
@@ -36,17 +37,11 @@ function deleteArticle(child) {
   ar.removeChild(child);
 }
 
-function newArt(artic) {
-  ar.appendChild(artic);
-}
-let aaa = [title.innerHTML, content.innerHTML];
-stringLocal('articles', aaa);
-
 //the article is stored in an array :new code
 let edit = document.querySelector('.fa-edit');
 edit.addEventListener('click', () => {
   let add = document.querySelector('.add');
-  console.log('huhu');
+
   if (add.style.display === 'none') {
     add.style.display = 'grid';
   } else {
@@ -59,10 +54,10 @@ function editt(key) {
   let jas = document.querySelector('#jas').value.trim();
   let js = document.querySelector('#js').value.trim();
 
-  arr.push(jas);
-  arr.push(js);
+  arr.unshift(jas);
+  arr.unshift(js);
 
-  nullP(arr[2], arr[3]);
+  nullP(arr[0], arr[1]);
   stringLocal(key, arr);
 }
 
@@ -78,7 +73,7 @@ edb.addEventListener('click', () => {
     });
     reader.readAsDataURL(this.files[0]);
   });
-  console.log(localStorage);
+
   let img = localStorage.getItem('image');
   picture.setAttribute('src', img);
 });
@@ -92,6 +87,42 @@ function apply() {
   aaaa[3].replaceChild(div, content);
 }
 
-console.log();
+let arti = JSON.parse(localStorage.getItem('article'));
+let ima = JSON.parse(localStorage.getItem('image'));
+console.log(arti);
 
-let delt = document.querySelector('fa-trash-alt');
+function design(a, b, d, z) {
+  let da = date.cloneNode();
+  let smash = document.querySelector('.smash');
+  let add = document.querySelector('.add');
+  let sma = smash.cloneNode('deep');
+  let addd = add.cloneNode('deep');
+
+  let dap = document.createElement('div');
+  let pic = document.createElement('img');
+  pic.classList.toggle('image');
+  pic.setAttribute('src', d);
+  da.innerHTML = z;
+  dap.append(da, pic);
+
+  let tc = document.createElement('div');
+  tc.classList.toggle('det');
+
+  let t = document.createElement('h2');
+  t.classList.toggle('title');
+  let c = document.createElement('p');
+  c.classList.toggle('content');
+  t = a;
+  c = b;
+
+  tc.append(t, c);
+  let ad = document.createElement('div');
+  ad.classList.toggle('article');
+  ad.append(dap, tc, sma, addd);
+
+  ar.append(ad);
+}
+
+for (let i = 0; i <= arti.length; i++) {
+  design(arti[i].title, arti[i].content, ima[i], arti[i].date);
+}
